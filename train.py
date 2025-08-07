661ded8553afc5bf2b360965e6e60efb9b558b44
吴子豪 <vortez.wohl@gmail.com>
2025-08-07 16:12:25 +0800

    update: 优化日志打印

--- train.py
+++ train.py
@@ -30,13 +30,18 @@ for i, name in enumerate(train_names):
     else:
         male_names.append(name)
 
-encoding_count = 0
+train_encoding_count = 0
+test_encoding_count = 0
 
 
-def encode(text: str):
-    global encoding_count
-    encoding_count += 1
-    print(f'\rEncoding ({encoding_count}/{len(train_names)}):', text, end='')
+def encode(text: str, train: bool = True):
+    global train_encoding_count, test_encoding_count
+    if train:
+        train_encoding_count += 1
+        print(f'\rEncoding ({train_encoding_count}/{len(train_names)}):', text, end='' * 32, flush=True)
+    else:
+        test_encoding_count += 1
+        print(f'\rEncoding ({test_encoding_count}/{len(test_names)}):', text, end='' * 32, flush=True)
     return ENCODER.encode(text)
 
 
@@ -115,7 +120,7 @@ except KeyboardInterrupt:
     model.eval()
     with torch.no_grad():
         for i, name in enumerate(test_names):
-            if round(model(encode(name)).item()) == test_genders[i]:
+            if round(model(encode(name, train=False)).item()) == test_genders[i]:
                 acc_count += 1.
     acc = 100 * (acc_count / len(test_names))
     print(f'Accuracy: {acc:.4f}%', flush=True)
